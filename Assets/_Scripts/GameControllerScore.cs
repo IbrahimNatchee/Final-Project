403bac02692d27800077cecb16a0cf66fd3bdf95
inatchee <300767752@cencol.ca>
2016-12-10 17:09:53 -0500

    progressing

---
+++ Assets/_Scripts/GameControllerScore.cs
@@ -0,0 +1,105 @@
+﻿/* Source File Name: GameController
+ * Author's Name: Ibrahim Natchee and Mamun Rahman
+ * Last Modified By: Ibrahim Natchee
+ * Date Modified Last: December 5th 2016
+ * Program Description: this file is GameController cs file for the game
+ * Revision History: December 5th 2016
+ */
+
+using UnityEngine;
+using System.Collections;
+
+// reference to the UI namespace
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// reference to manage my scenes
+using UnityEngine.SceneManagement;
+
+
+/**  
+* <summary>  
+* This is the GameController class to control the Game.  
+* </summary>  
+*   
+* @class EnemyController  
+*/
+public class GameControllerScore : MonoBehaviour
+{
+    // PRIVATE INSTANCE VARIABLES ++++++++++++++++++
+    private int _livesValue;
+    private int _scoreValue;
+
+    [Header("UI Objects")]
+    public Text LivesLabel;
+    public Text ScoreLabel;
+
+    // PUBLIC PROPERTIES +++++++++++++++++++++++++++
+    public int LivesValue
+    {
+        get
+        {
+            return this._livesValue;
+        }
+
+        set
+        {
+            this._livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                this.LivesLabel.text = "Lives: 0";
+                SceneManager.LoadScene("GameOver");
+
+            }
+            else
+            {
+                this.LivesLabel.text = "Lives: " + this._livesValue;
+            }
+        }
+    }
+
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+            this._scoreValue = value;
+            this.ScoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+
+
+    /**
+        * <summary>
+        * This is the method for starting the EnemyController class which initiates value
+        * </summary>
+        * 
+        * @method Start
+        * @returns {void} 
+        */
+    void Start()
+    {
+        this.LivesValue = 5;
+        this.ScoreValue = 0;
+
+    }
+
+    /**
+        * <summary>
+        * This method is called once per frame.
+        * </summary>
+        * 
+        * @method Update
+        * @returns {void} 
+        */
+    void Update()
+    {
+    }
+
+
+}
---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,98 @@
+﻿/* Source File Name: GameController
+ * Author's Name: Ibrahim Natchee and Mamun Rahman
+ * Last Modified By: Ibrahim Natchee
+ * Date Modified Last: December 5th 2016
+ * Program Description: Controls player movement and mechanics
+ * Revision History: December 5th 2016
+ 
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerShooting : MonoBehaviour {
+
+	// PUBLIC VARIABLES FOR TESTING
+	public Transform FlashPoint;
+	public GameObject MuzzleFlash;
+	public GameObject Explosion;
+	public GameObject BulletImpact;
+	public AudioSource RifleShotSound;
+	public Transform PlayerCam;
+
+    // PRIVATE VARIABLES
+
+        //testing
+    /*private GameObject _gameControllerObject;
+    private GameControllerScore _gameControllerScore;*/
+    
+    
+    // Use this for initialization
+    void Start () {
+
+
+        //testing
+       /* this._gameControllerObject = GameObject.Find("GameControllerScore");
+        this._gameControllerScore = this._gameControllerObject.GetComponent<GameControllerScore>() as GameControllerScore;*/
+
+    }
+	
+	// Update is called once per frame (for Physics)
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {
+			// show the MuzzleFlash at the FlashPoint without any rotation
+			Instantiate (this.MuzzleFlash, this.FlashPoint.position, Quaternion.identity);
+
+			// need a variable to hold the location of our Raycast Hit
+			RaycastHit hit;
+
+			// if raycast hits an object then do something...
+			if (Physics.Raycast (this.PlayerCam.position, this.PlayerCam.forward, out hit)) {
+
+				if (hit.transform.gameObject.CompareTag ("Alien")) {
+					Instantiate (this.Explosion, hit.point, Quaternion.identity);
+                    //testing
+                    //this._gameControllerScore.ScoreValue = this._gameControllerScore.ScoreValue + 10;
+                    Destroy (hit.transform.gameObject);
+				}
+                 else if (hit.transform.gameObject.CompareTag("Troll"))
+                {
+                    Instantiate(this.Explosion, hit.point, Quaternion.identity);
+                    //testing
+                    //this._gameControllerScore.ScoreValue = this._gameControllerScore.ScoreValue + 10;
+                    Destroy(hit.transform.gameObject);
+                }
+
+
+                else
+                {
+					Instantiate (this.BulletImpact, hit.point, Quaternion.identity);
+				}
+
+
+			}
+
+			// Play Rifle Sound
+			this.RifleShotSound.Play();
+		}
+	}
+
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Exit")) { 
+        SceneManager.LoadScene("SecondLevel");
+        }
+        //testing
+       /* if (other.gameObject.CompareTag("Alien"))
+        {
+            this._gameControllerScore.LivesValue = this._gameControllerScore.LivesValue - 1;
+        }
+        if (other.gameObject.CompareTag("Tikki"))
+        {
+            this._gameControllerScore.ScoreValue = this._gameControllerScore.ScoreValue + 20;
+        }*/
+    }
+
+   
+}
